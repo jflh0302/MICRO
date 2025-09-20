9befbbc20738af005bed24e60b63fd67fba9bd5f
jflh0302 <jflh0302@gmail.com>
2025-09-20 05:32:46 -0600

    Delete l_02 directory

--- l_02/Objects/system_stm32f10x.d
+++
@@ -1,5 +0,0 @@
-./objects/system_stm32f10x.o: RTE\Device\STM32F103C8\system_stm32f10x.c \
-  C:\Users\jflh0\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
-  RTE\_alu\RTE_Components.h \
-  C:\Users\jflh0\AppData\Local\Arm\Packs\ARM\CMSIS\6.2.0\CMSIS\Core\Include\core_cm3.h \
-  C:\Users\jflh0\AppData\Local\Arm\Packs\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h
